2c4476423f112b71f71bf181a69ce0c5b1cccbb9
Samuel <samukahuss2009@gmail.com>
2025-11-22 11:23:28 -0300

    feat: initial commit

---
+++ backend/mqtt/client.go
@@ -0,0 +1,37 @@
+package mqtt
+
+import (
+	"fmt"
+	"log"
+
+	mqtt "github.com/eclipse-paho/paho.mqtt.golang"
+)
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	log.Println("Connected to MQTT broker")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	log.Printf("Connection to MQTT broker lost: %v", err)
+}
+
+func NewClient(broker, clientID string) mqtt.Client {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s", broker))
+	opts.SetClientID(clientID)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+	client := mqtt.NewClient(opts)
+	token := client.Connect()
+
+	if token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	return client
+}
+
+// Packages: The package mqtt declaration at the top must match the name of the directory the file is in. This is how Go organizes code.
+// Callbacks: The OnConnect and OnConnectionLost handlers are a great example of a common programming pattern. You give another piece of code (the MQTT library) a function to execute when a specific event occurs.
+// Asynchronous Operations: client.Connect() doesn't wait. It starts the work in the background. The token it returns is an object that lets you check the status of that work later, for example by calling token.Wait().
+// Error Handling: The if token.Wait() && token.Error() != nil block is a standard way to handle errors from asynchronous operations in this library.
---
+++ backend/websocket/hub.go
@@ -0,0 +1,84 @@
+package websocket
+
+import (
+	"log"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	proto "github.com/samukahuss/go-websocket-mqtt/internal/proto"
+)
+
+type TargeredMessage struct {
+	TargetID string
+	Payload  byte
+}
+
+type Hub struct {
+	// map com k=clientId v=clientObj
+	clients map[string]*Client
+	// pra receber menssagens de um unico client
+	Unicast chan *TargeredMessage
+	// um channel pra novos clients
+	register chan *Client
+	// channel pra remover clients desconectados
+	unregister chan *Client
+	// guardando uma referencia do MQTT client pra publicar menssagens
+	mqttClient mqtt.Client
+	mqttTopic  string
+}
+
+// NewHub eh o consrutor de Hub
+func NewHub(mqttClient mqtt.Client, topic string) *Hub {
+	return &Hub{
+		Unicast:    make(chan *TargeredMessage),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[string]*Client),
+		mqttClient: mqttClient,
+		mqttTopic:  topic,
+	}
+}
+
+// loop principal
+func (h *Hub) Run() {
+	for {
+		select {
+		// Case 1: client connecta
+		case client := <-h.register:
+			h.clients[client.ID] = client
+			log.Printf("Client %s registered", client.ID)
+
+		case client := <-h.unregister:
+			// verificando se o client existe
+			if _, ok := h.clients[client.ID]; ok {
+				delete(h.clients, client.ID)
+				close(client.send)
+				log.Printf("Client %s unregistered")
+			}
+		case message := <-h.Unicast:
+
+			client, ok := h.clients[message.TargetID]
+
+			if ok {
+				// Se existe um client, envia mensagem pro seu canal
+				// Estou usando um select exclusivo pra casos onde o canal possa estar cheio
+				select {
+				case client.send <- message.Payload:
+				// se nao consegui enviar estou assumindo que o canal morreu
+				default:
+					close(client.send)
+					delete(h.clients, client.ID)
+				}
+			} else {
+				log.Printf("Client %s not found for unicast message", message.TargetID)
+			}
+
+			var msg proto.WebSocketMessage
+
+			if err := proto.Unmarshal(message.Payload, &msg); err == nil {
+				log.Printf("Publishing message type '%s' to MQTT topic '%s'", msg.Type, h.mqttTopic)
+				h.mqttClient.Publish(h.mqttTopic, 0, false, message.Payload)
+			}
+
+		}
+	}
+}
